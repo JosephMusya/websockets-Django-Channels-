a8531bf706be513fe805a23ba4e3ac90b65531c8
JosephMusya <josephmusya254@gmail.com>
2022-04-22 18:09:22 +0300

    added user model

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+
+class Values(models.Model):
+    integer = models.IntegerField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
