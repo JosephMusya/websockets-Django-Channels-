1764da1baf2abd41cd421723651642d7d1798e81
JosephMusya <josephmusya254@gmail.com>
2022-04-22 18:10:20 +0300

    Added user and value serializers

---
+++ serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Values
+from django.contrib.auth.models import User
+class ValueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Values
+        fields = [
+            'integer','user'
+        ]
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'username'
+        ]
