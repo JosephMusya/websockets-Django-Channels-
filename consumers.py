4c5b218fb5442040e127de984e531f7dc8136091
JosephMusya <josephmusya254@gmail.com>
2022-04-22 11:00:33 +0300

    Updated to use asynchronous webscoks

--- consumers.py
+++ consumers.py
@@ -1,46 +1,48 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-from asgiref.sync import async_to_sync
+#from asgiref.sync import async_to_sync
 
-class MySocket(WebsocketConsumer):
-    def connect(self):
-        
+class MySocket(AsyncWebsocketConsumer):
+    async def connect(self):
+        print("PATH",self.scope['path'])
+        print("USER",self.scope['user'])
+
         self.room_group_name = 'test'
-        async_to_sync(self.channel_layer.group_add)(
+        await (self.channel_layer.group_add)(
             self.room_group_name,
-            self.channel_name
+            self.channel_name #Auto generated
         )
         
-        self.accept()
-        self.send(text_data=json.dumps({
+        await self.accept()
+        await self.send(text_data=json.dumps({
             'type':'connection-response',
             'message':'connection accepted from host',
         }))
         
         
-    def receive(self,text_data):
+    async def receive(self,text_data):
         json_data = json.loads(text_data)
         info = json_data['message']
         print("Message: ",info)
         
-        self.send(text_data = json.dumps({
+        await self.send(text_data = json.dumps({
             'type':'response',
             'message':info
         }))
         
-        async_to_sync(self.channel_layer.group_send)(
+        await (self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type':'Another_message',
                 'message':info
             }
         )
-    def Another_message(self,event):
+    async def Another_message(self,event):
         message = event['message']
         
-        self.send(text_data = json.dumps({
+        await self.send(text_data = json.dumps({
             'type':'response',
             'message':message
         }))
