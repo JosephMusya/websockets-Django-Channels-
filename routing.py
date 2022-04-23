64f3740e8b01b38fdb679bc350bd96a2330bc73f
JosephMusya <josephmusya254@gmail.com>
2022-04-23 19:47:42 +0300

    Consumer router

--- routing.py
+++ routing.py
@@ -1,5 +1,7 @@
 from django.urls import re_path
+from . import views
 from .consumers import MySocket
+
 websocket_urlpatterns = [    
-    re_path(r'ws/', MySocket.as_asgi())
+    re_path(r'ws/', MySocket.as_asgi()),
 ]
