f35dd4ee474a8386e5f243b62e7c57080f543258
JosephMusya <josephmusya254@gmail.com>
2022-04-23 19:45:40 +0300

    Added pure REST API functionality

--- views.py
+++ views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from .serializers import ValueSerializer
+from .models import Values
+from rest_framework import generics
+
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+class MySocket(generics.ListCreateAPIView,):
+    queryset = Values.objects.all()
+    serializer_class = ValueSerializer
+    
+    
