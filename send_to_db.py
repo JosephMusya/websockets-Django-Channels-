27b458992c16a8b6dcf912143a061482a5f29275
JosephMusya <josephmusya254@gmail.com>
2022-04-23 19:52:06 +0300

    Posting to backend

---
+++ send_to_db.py
@@ -0,0 +1,23 @@
+import requests
+
+# LOGIN_TOKEN = 'be590b7a4c26e54091d6d83ee6b85e157b058000'
+# AUTH = {
+#         'Authorization': 'Token {}'.format(LOGIN_TOKEN)
+#     }
+
+url = 'http://localhost:8000/value/1'
+
+PARAMS = {
+    'integer':90,
+}
+try:
+    for i in range(0,90,1):
+        new_response = requests.post(url,
+                                    json={
+                                        'integer':i,
+                                        })
+        print("Sending...")
+    # print(new_response.json()['integer'])
+
+except Exception as e:
+    print("ERROR",e)
