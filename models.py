63d28f9e6fc0f67c049914556801e95e1672bb14
JosephMusya <josephmusya254@gmail.com>
2022-04-27 19:04:43 +0300

    Changed integer value to baggage status

--- models.py
+++ models.py
@@ -5,6 +5,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Values(models.Model):
-    integer = models.IntegerField(null=True)
+    status = (
+        ('Checked','Checked'),
+        ('Scanned','Scanned'),
+        ('Screened','Screened'),
+        ('Make-up','Make-up'),
+        ('To Loading','To Loading'),
+        ('Loaded','Loaded')
+    )
+    baggage_status = models.CharField(max_length=50,
+                              choices=status,
+                              default='Checked')
+    #status = models.IntegerField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     
+    def __str__(self):
+        return self.baggage_status +" > "+ self.user.username
